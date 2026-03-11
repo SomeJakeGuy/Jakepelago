bdda6257f7ed2e61dc00a15fde76d0bb5c2dd4e4
SomeJakeGuy <somejakeguy@gmail.com>
2026-03-11 07:00:19 -0400

    Re-worked init a bit to load all json tables as a class var, then create the various other required class vars included location/item groupings.

--- worlds/forager/__init__.py
+++ worlds/forager/__init__.py
@@ -1,47 +1,78 @@
-from worlds.AutoWorld import World, WebWorld
-from BaseClasses import ItemClassification as IClass
-from worlds.LauncherComponents import Component, components, icon_paths, launch as launch_component, Type
-from .Regions import load_tables, load_regions, ForagerLocation
-from.Items import ForagerItem
+from typing import ClassVar
 
+from worlds.AutoWorld import World
+from worlds.LauncherComponents import Component, components, icon_paths, launch as launch_component, Type
+from BaseClasses import ItemClassification as IClass, Item
+
+from .forager_constants import GAME_NAME, CLIENT_NAME
+from .forager_webworld import ForagerWebWorld
+from .helper_functions import load_tables, load_json_tables
+from .forager_regions import load_regions
+
+
+# Define the client launch state function and add it to the AP Launcher
 def launch_client():
     from .Client import launch
-    launch_component(launch, name="ForagerClient")
+    launch_component(launch, name=CLIENT_NAME)
+components.append(Component(CLIENT_NAME, func=launch_client, component_type=Type.CLIENT))
 
 
-components.append(Component("Forager Client", func=launch_client,
-                            component_type=Type.CLIENT))
-
-#Class ForagerWebWorld(Webworld): //Major
 class ForagerWorld(World):
-    game = "Forager"
-    item_name_to_id = {}
-    location_name_to_id = {}
-    load_tables(item_name_to_id,location_name_to_id) #Placeholder, will probably need to be done elsewhere
-    #web = ForagerWebWorld()
+    game = GAME_NAME
+    item_name_to_id: ClassVar[dict[str, int]]
+    location_name_to_id: ClassVar[dict[str, int]]
+    json_tables: dict[str, dict] = load_json_tables()
+
+    item_name_to_id, item_name_groups, location_name_to_id, location_name_groups = (
+        load_tables(json_tables["items"], json_tables["locations"]))
+
+    web = ForagerWebWorld()
 
     def __init__(self, multiworld, player):
         super().__init__(multiworld, player)
 
-    def create_item(self, name):
-        return ForagerItem(name,IClass.progression,self.item_name_to_id[name],self.player) #Needs to have item classifications done
-    
-    def create_regions(self):
-        regions = load_regions(self.player,self.multiworld)
-        for location,address in self.location_name_to_id.items():
-            regions["Menu"].locations.append(ForagerLocation(self.player,location,address, parent = regions["Menu"])) #Placeholder
-        self.multiworld.regions.extend(list(regions.values()))
-    
-    def create_items(self):
-        #Massive placeholder
-        locations = len(self.location_name_to_id)
-        items = 0
-        for item,address in self.item_name_to_id.items():
-            if(items < locations):
-                self.multiworld.itempool.append(ForagerItem(item,IClass.progression,address,self.player))
-                items += 1
-        for i in range(items,locations):
-            self.multiworld.itempool.append(ForagerItem("Nothing!",IClass.progression,364,self.player))
+    def generate_early(self) -> None:
+        """Used for evaluating any OptionErrors we see fit, updating some option values based on invalid combos, etc."""
+        pass
 
-    
-    
+
+    def create_regions(self):
+        """Loads both the regions and the locations"""
+        load_regions(self)
+
+
+    def create_rules(self):
+        """Attach the various rules for both locations and regions"""
+        # TODO move the region logic create to here, potentially make a new function to iterate through regions again.
+        pass
+
+
+    def create_items(self):
+        """Creates the various items required based on the user's options"""
+        item_pool: list[Item] = []
+
+        for category_name, category in self.json_tables["items"].items():
+            if category_name == "Misc":
+                continue
+
+            for item_name, item_id in category.items():
+                item_pool.append(Item(item_name, IClass.progression, item_id["id"] if
+                    category_name == "Tools" else item_id, self.player))
+
+        # Calculate the number of progression items required vs the number of unfilled locations left.
+        # Create that many of filler items remaining.
+        locations_left_to_fill: int = len(self.multiworld.get_unfilled_locations(self.player)) - len(item_pool)
+        for loc_to_fill in range(locations_left_to_fill):
+            # Pick a random filler item and add that to the item pool
+            random_filler: str = self.random.choice(list(self.json_tables["items"]["Misc"].keys()))
+            item_pool.append(Item(random_filler, IClass.filler,
+                self.json_tables["items"]["Misc"][random_filler], self.player))
+
+        self.multiworld.itempool += item_pool
+
+
+    def create_item(self, name):
+        # TODO get the item from item categories and see if its not in Misc. If not misc, progression for now, else filler.
+        pass
+        #return ForagerItem(name, IClass.progression, self.item_name_to_id[name],
+        #                   self.player)  # Needs to have item classifications done
