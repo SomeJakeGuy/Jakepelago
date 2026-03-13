947140fcdc07f39646df6865b7eb517dd5959a93
SomeJakeGuy <somejakeguy@gmail.com>
2026-03-12 22:41:20 -0400

    Created LevelGroup enum for various level groups. Added those to the regions to be created and created their region_access requirements.

--- worlds/forager/forager_regions.py
+++ worlds/forager/forager_regions.py
@@ -1,9 +1,21 @@
+from enum import StrEnum
 from typing import TYPE_CHECKING, NamedTuple
 
-from BaseClasses import Region, Entrance, CollectionState
+from BaseClasses import Region
 
 if TYPE_CHECKING:
     from worlds.forager import ForagerWorld
+
+
+class LevelGroups(StrEnum):
+    FirstGroup = "Levels 1-5"
+    SecondGroup = "Levels 6-10"
+    ThirdGroup = "Levels 11-20"
+    FourthGroup = "Levels 21-30"
+    FifthGroup = "Levels 31-40"
+    SixthGroup = "Levels 41-50"
+    SeventhGroup = "Levels 51-60"
+    EighthGroup = "Levels 61-65"
 
 
 class ForagerRegionData(NamedTuple):
@@ -20,6 +32,15 @@ region_access: dict[str, ForagerRegionData] = {
     "Void Steel": ForagerRegionData("Electronics", ["Foraging", "Sewing", "Summoning", "Transmutation", "Spirituality"]),
     "Cosmic Steel": ForagerRegionData("Void Steel", ["Astrology"]),
     "Nuclear": ForagerRegionData("Void Steel", ["Physics"]),
+
+    str(LevelGroups.FirstGroup.name): ForagerRegionData("Menu"),
+    str(LevelGroups.SecondGroup.name): ForagerRegionData(str(LevelGroups.FirstGroup.name), ["Industry"]),
+    str(LevelGroups.ThirdGroup.name): ForagerRegionData(str(LevelGroups.SecondGroup.name)),
+    str(LevelGroups.FourthGroup.name): ForagerRegionData(str(LevelGroups.ThirdGroup.name)),
+    str(LevelGroups.FifthGroup.name): ForagerRegionData(str(LevelGroups.FourthGroup.name)),
+    str(LevelGroups.SixthGroup.name): ForagerRegionData(str(LevelGroups.FifthGroup.name)),
+    str(LevelGroups.SeventhGroup.name): ForagerRegionData(str(LevelGroups.SixthGroup.name)),
+    str(LevelGroups.EighthGroup.name): ForagerRegionData(str(LevelGroups.SeventhGroup.name)),
 }
 
 def load_regions(world: "ForagerWorld"):
@@ -28,9 +49,10 @@ def load_regions(world: "ForagerWorld"):
         world.multiworld.regions.append(Region(region_name, world.player, world.multiworld))
 
     if world.options.game_mode.value == world.options.game_mode.option_default:
-        # TODO create logical groups for locations based on group A up to X xp, then do some other groups.
-        #   Formulas to calculate xp per region to follow in rules.py
-        pass
+        for lvl_enum in LevelGroups:
+            world.multiworld.regions.append(Region(str(lvl_enum.name), world.player, world.multiworld))
+            # TODO Formulas to calculate xp per region to follow in rules.py
+            pass
 
 
 def create_locations(world: "ForagerWorld"):
