7519e9b9f51e4870465f990fa5d278d060d9ecfd
SomeJakeGuy <somejakeguy@gmail.com>
2026-03-23 04:43:46 -0400

    Fixed create_item to properly search and return the item as expected. Added UT support in preparation for some testing.

--- worlds/forager/__init__.py
+++ worlds/forager/__init__.py
@@ -1,11 +1,13 @@
 from typing import ClassVar
+from dataclasses import fields
 
-import Utils
+from Options import PerGameCommonOptions
 from worlds.AutoWorld import World
+from BaseClasses import ItemClassification
 from worlds.LauncherComponents import Component, components, icon_paths, launch as launch_component, Type
 
 from .forager_constants import GAME_NAME, CLIENT_NAME
-from .forager_items import create_world_items
+from .forager_items import create_world_items, ForagerItem
 from .forager_rules import create_region_access_rules, create_location_access_rules
 from .forager_webworld import ForagerWebWorld
 from .helper_functions import load_tables, load_json_tables
@@ -39,14 +41,27 @@ class ForagerWorld(World):
     web = ForagerWebWorld()
     required_level_count: int
 
+    ut_can_gen_without_yaml = True  # class var that tells UT that it can gen using slot data instead of player yamls
+
     def __init__(self, multiworld, player):
         super().__init__(multiworld, player)
         self.required_level_count = 0
 
     def generate_early(self) -> None:
         """Used for evaluating any OptionErrors we see fit, updating some option values based on invalid combos, etc."""
+
+        # Load UT's data from slot data to avoid incorrect logic being presented to the player
+        if hasattr(self.multiworld, "re_gen_passthrough"):
+            slot_data = self.multiworld.re_gen_passthrough[self.game]
+            for key, value in slot_data.items():
+                try:
+                    getattr(self.options, key).value = value
+                except AttributeError:
+                    # As there might be slot_data keys that were added that are not options, just ignore the error.
+                    pass
+
         if self.options.game_mode.value == self.options.game_mode.option_default:
-            self.required_level_count = 65
+            self.required_level_count = self.options.required_level.value
 
         self.multiworld.early_items[self.player].update({"Industry": 1})
 
@@ -59,7 +74,6 @@ class ForagerWorld(World):
 
     def set_rules(self):
         """Attach the various rules for both locations and regions"""
-        # TODO make location rules as well.
         create_region_access_rules(self)
         create_location_access_rules(self)
 
@@ -70,16 +84,27 @@ class ForagerWorld(World):
 
 
     def create_item(self, name):
-        # TODO get the item from item categories and see if its not in Misc. If not misc, progression for now, else filler.
-        pass
-        #return ForagerItem(name, IClass.progression, self.item_name_to_id[name],
-        #                   self.player)  # Needs to have item classifications done
+        for item_type in self.item_class_sets.keys():
+            if name in self.item_class_sets[item_type].keys():
+                cat_name: str = self.item_class_sets[item_type][name]
+                return ForagerItem(name, ItemClassification(str(item_type).lower()), self.item_name_to_id[name], self.player)
+
+        raise Exception(f"Forager world is unable to find an item named: {name}")
 
 
-    def generate_basic(self) -> None:
-        print("")
+    def fill_slot_data(self):
+        slot_data: dict = {}
 
+        # This gets all the names of the options from both the world's option class and the inherited class as sets
+        # Since this world's options inherit from PerGameCommonOptions, there will be duplicates.
+        child_local_fields = set({f.name for f in fields(ForagerOptions)})
+        parent_fields = set({f.name for f in fields(PerGameCommonOptions)})
 
-    def generate_output(self, output_directory: str) -> None:
-        Utils.visualize_regions(self.get_region("Menu"), "forager.puml", show_entrance_names=True, show_locations=True,
-            show_other_regions=True, detail_other_regions=True)
+        # Subtract the elements to find the options only unique to this world, then sort them to avoid any
+        #   deterministic issues.
+        only_in_child = sorted(list(child_local_fields - parent_fields))
+
+        # Output the value of each based on the option type.
+        for child_option in only_in_child:
+            slot_data[child_option] = getattr(self.options, child_option).value
+        return slot_data
