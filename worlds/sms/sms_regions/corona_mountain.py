06c9390e5ee242b6b6232abbc2ef3a9205507894
Spoider1315 <spiderhawk1315@gmail.com>
2026-03-29 11:53:36 -0500

    Added difficulty for corona mountain blue coins

--- worlds/sms/sms_regions/corona_mountain.py
+++ worlds/sms/sms_regions/corona_mountain.py
@@ -1,16 +1,70 @@
 from .sms_region_helper import *
 
-CORONA_MOUNTAIN: SmsRegion = SmsRegion(SmsRegionName.CORONA,
-    requirements=[Requirements(SPRAY_AND_HOVER, corona=True)],
-    blue_coins=[BlueCoin("Platform", in_game_bit=540),
-        BlueCoin("Back Right Lava", in_game_bit=541),
-        BlueCoin("Left Lava", in_game_bit=542),
-        BlueCoin("Front Lava", in_game_bit=543),
-        BlueCoin("Front Left Lava", in_game_bit=544),
-        BlueCoin("Front Right Lava", in_game_bit=545),
-        BlueCoin("Back Left Lava", in_game_bit=546),
-        BlueCoin("Far Back Left Lava", in_game_bit=547),
-        BlueCoin("Far Back Right Lava", in_game_bit=548),
-        BlueCoin("Right Lava", in_game_bit=549)],
+CORONA_MOUNTAIN: SmsRegion = SmsRegion(
+    SmsRegionName.CORONA,
+    requirements=[Requirements(corona=True)],
+    blue_coins=[
+        BlueCoin(
+            "Platform",
+            requirements=Requirements([[NozzleType.hover]]),
+            tears=Requirements(SPRAY_OR_HOVER_OR_ROCKET),
+            in_game_bit=540,
+        ),
+        BlueCoin(
+            "Back Right Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=541,
+        ),
+        BlueCoin(
+            "Left Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=542,
+        ),
+        BlueCoin(
+            "Front Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=543,
+        ),
+        BlueCoin(
+            "Front Left Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=544,
+        ),
+        BlueCoin(
+            "Front Right Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=545,
+        ),
+        BlueCoin(
+            "Back Left Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=546,
+        ),
+        BlueCoin(
+            "Far Back Left Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=547,
+        ),
+        BlueCoin(
+            "Far Back Right Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=548,
+        ),
+        BlueCoin(
+            "Right Lava",
+            requirements=Requirements(SPRAY_AND_HOVER),
+            tears=Requirements(manual_none=True),
+            in_game_bit=549,
+        ),
+    ],
     nozzle_boxes=[NozzleBox("Rocket Box", in_game_bit=886)],
-    parent_region=SmsRegionName.PLAZA)
+    parent_region=SmsRegionName.PLAZA,
+)
